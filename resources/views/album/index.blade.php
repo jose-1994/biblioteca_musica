cbe1c4747a0304eafbfcd9125f63e66e52cebcc9
jose-1994 <jose_011594@hotmail.com>
2023-06-19 01:52:46 -0600

    DataTable

--- resources/views/album/index.blade.php
+++ resources/views/album/index.blade.php
@@ -1,9 +1,15 @@
 <x-layout>
  @extends('layouts.plantillabase');
 
+ @section('css')
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
+ <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
+ @endsection
+
  @section('contenido')
- <a href="album/create" class="btn btn-primary">Agregar Album</a>
- <table class="table table-dark table-striped mt-4">
+ <a href="album/create" class="btn btn-primary mb-3">Agregar Album</a>
+
+ <table class="table table-dark table-striped mt-4" id="tabla">
     <thead>
         <tr>
             <th scope="col">ID</th>
@@ -33,6 +39,14 @@
         @endforeach
     </tbody>
  </table>
+
  @endsection
 </x-layout>
-
+@section('js')
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+    <script>
+    $('#tabla').DataTable();
+    </script>
+ @endsection
