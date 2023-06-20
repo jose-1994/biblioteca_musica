4022cc3f4123db388c0f4b94af8e9bf3d1b71a62
jose4991 <79180870+jose4991@users.noreply.github.com>
2023-06-19 20:28:10 -0600

    dt

--- resources/views/album/index.blade.php
+++ resources/views/album/index.blade.php
@@ -47,6 +47,9 @@
     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
     <script>
-    $('#tabla').DataTable();
+    $('#tabla').DataTable({
+        responsive: true,
+        autoWidth: false
+    });
     </script>
  @endsection
