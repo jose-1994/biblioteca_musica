cdc3b71a3c5de0920e19ce7faed723e214d1e9e5
jose4991 <79180870+jose4991@users.noreply.github.com>
2023-06-22 22:51:48 -0600

    x

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<meta charset="utf-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página de Presentación</title>
@@ -81,10 +84,31 @@ footer {
     <header>
         <nav>
             <ul>
+            <div class="dropdown">
                 <li><a href="{{url('/')}}">Inicio</a></li>
-                <li><a href="{{url('/album')}}">Album</a></li>
-                <li><a href="#">Servicios</a></li>
-                <li><a href="#">Contacto</a></li>
+ <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown">
+    Artistas
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="{{url('/artistas')}}">Mostrar Artistas</a></li>
+    <li><a class="dropdown-item" href="{{url('/artistas/create')}}">Crear Artistas</a></li>
+  </ul>
+
+  <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown">
+    Albums
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="{{url('/album')}}">Mostrar Albums</a></li>
+    <li><a class="dropdown-item" href="{{url('/album/create')}}">Crear Albums</a></li>
+  </ul>
+  <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown">
+    
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="{{url('/artistas')}}">Mostrar Artistas</a></li>
+    <li><a class="dropdown-item" href="{{url('/artistas/create')}}">Crear Artistas</a></li>
+  </ul>
+
            @if (Route::has('login'))
             @auth
                 <li><a href="{{ url('/dashboard') }}">Dashboard</a></li>
@@ -96,6 +120,8 @@ footer {
                 @endif
             @endauth
         @endif</ul>
+        </div>
+
         </nav>
     </header>
             {{ $slot }}
@@ -104,5 +130,6 @@ footer {
         <!-- Contenido del pie de página -->
         <p>Derechos de autor &copy; 2023. Todos los derechos reservados.</p>
     </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 </html>
