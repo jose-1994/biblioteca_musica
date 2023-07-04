2c23924f87f0e603f532d4f453968d38f9ee3204
Doom11797 <133293823+Doom11797@users.noreply.github.com>
2023-07-03 20:07:56 -0600

    edit

--- resources/views/artistas/edit.blade.php
+++ resources/views/artistas/edit.blade.php
@@ -8,15 +8,15 @@
 @method('PUT')
 <div class="mb-3">
 <label for=""class="form-label">Nombre</label>
-<input id="nombre" name="nombre" type="text" class="form-control" tabindex="1">
+<input id="nombre" name="nombre" type="text" value="{{$artista->nombre}}" class="form-control" tabindex="1">
 </div>
 <div class="mb-3">
 <label for=""class="form-label">Pais</label>
-<input id="pais" name="pais" type="text" class="form-control" tabindex="2">
+<input id="pais" name="pais" value="{{$artista->pais}}" type="text" class="form-control" tabindex="2">
 </div>
 <div class="mb-3">
 <label for=""class="form-label">Genero</label>
-<input id="genero" name="genero" type="text" class="form-control" tabindex="3">
+<input id="genero" name="genero" value="{{$artista->genero}}" type="text" class="form-control" tabindex="3">
 </div>
 <a href="/artistas" class="btn btn-secondary" tabindex="5">Cancelar</a>
 <button type="submit" class="btn btn-primary" tabindex="4">Guardar</button>
