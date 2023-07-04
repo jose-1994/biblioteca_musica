c3255629ff3295c58d9d2d7f71dc7f33e8d56e9f
jose-1994 <jose_011594@hotmail.com>
2023-07-03 21:39:48 -0600

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,37 +1,59 @@
 <x-layout>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+        <style>
+        h1{
+        text-align: center;
+        text-transform: uppercase;
+        }
+        .contenido{
+        font-size: 20px;
+        }
+        #primero{
+        background-color: #ccc;
+        }
+        #segundo{
+        color:#44a359;
+        }
+        #tercero{
+        text-decoration:line-through;
+        }
+    </style>
+    </head>
+    <body>
+      <div class="container col-10">
+  <center>
 
-<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
-  <center>
 
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="/img/nirvana.png" class="d-block w-40 " alt="40">
+      <img src="/img/nirvana.png" class="d-block w-25" alt="">
       <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
+        <h5>Nirvana</h5>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="/img/kizz.png" class="d-block w-40 " alt="40">
+      <img src="/img/kizz.png" class="d-block w-25" alt="">
       <div class="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
+        <h5>KIZZ</h5>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="/img/soda.png" class="d-block w-40 " alt="40">
+      <img src="/img/soda.png" class="d-block w-25" alt="">
       <div class="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
+        <h5>Soda Stereo</h5>
       </div>
     </div>
   </div>
-  </center>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
@@ -40,5 +62,11 @@
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
+  </center>
+
 </div>
+</div>
+</div>
+    </body>
+</html>
 </x-layout>
